6cd6723cc0dc7ea419fc7b806b6f4fdbf3590366
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2021-10-11 22:58:13 -0300

    Criado esqueleto de cadastro do usuário

---
+++ src/views/CadastroUsuario.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
