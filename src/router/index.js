d129bda7121603c1aa379b06000f575a0ccf5f7a
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2021-10-11 23:00:40 -0300

    ------

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import CupomPromocional from "../views/CupomPromocional.vue";
+import CadastroCupom from "../views/CadastroCupom.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+  { path: "/", name: "Login", component: Login },
+  { path: "/home", name: "Home", component: Home },
+  { path: "/cupom-promocional", component: CupomPromocional },
+  { path: "/cadastro-cupom", component: CadastroCupom },
 ];
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <app-nav></app-nav>
+
+    <div class="container-fluid">
+      <!-- Control the column width, and how they should appear on different devices -->
+      <div class="row">
+        <div class="col-sm-4 ml-4 mt-4">
+          <app-menu></app-menu>
+        </div>
+        <div class="col-sm-4"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from "../components/nav/Nav.vue";
+import Menu from "../components/menu/Menu.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    "app-nav": Navbar,
+    "app-menu": Menu,
   },
 };
 </script>
