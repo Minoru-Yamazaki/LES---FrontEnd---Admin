2db816321db839f2923a7a92cb463153ee9dcecf
Minoru-Yamazaki <minoru.rhcp@gmail.com>
2021-10-17 10:43:54 -0300

    Adicionado no método excluirCookie a remoção do idCupom

--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -46,6 +46,7 @@ export default {
     exluirCookies() {
       localStorage.removeItem("login");
       localStorage.removeItem("admin");
+      localStorage.removeItem("idCupom");
     },
   },
 };
